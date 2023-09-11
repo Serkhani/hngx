2687f7557a6b80ec9a67c8b2573095f1d8d5b1db
Serkhani <deviper03@gmail.com>
2023-09-11 22:05:52 +0100

    added color

--- zuri_stage_one/lib/homepage.dart
+++ zuri_stage_one/lib/homepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -7,19 +6,35 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              CircleAvatar(
-                // foregroundImage: AssetImage(),
-              ),
-              Text("Boniface Delali Dakey"),
-              ElevatedButton(onPressed: () {}, child: Text("Open Github"))
-            ],
+        body: Container(
+          color: Colors.lightBlueAccent,
+          width: double.infinity,
+          height: double.infinity,
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius: 70.0,
+                    foregroundImage: AssetImage('assets/profile.jpg'),
+                  ),
+                ),
+                const Text("Boniface Delali Dakey",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w500,
+                  overflow: TextOverflow.fade
+                ),),
+                    ElevatedButton(
+                      onPressed: () {},
+                       child: const Text("Open Github"),
+                    ),
+              ],
+            ),
           ),
         ),
       ),
